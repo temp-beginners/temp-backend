ac1310d0fd64e709538d4d6c9b025561d076dce2
Taeyang Jin (Heli) <sun@jupiterflow.com>
2022-09-26 00:21:42 +0900

    feat: Account 도메인 패키지 변경

--- commons/persistence-database/src/main/kotlin/kr/dataportal/application/persistence/entity/account/UserAccount.kt
+++ commons/persistence-database/src/main/kotlin/kr/dataportal/application/persistence/entity/account/UserAccount.kt
@@ -1,4 +1,4 @@
-package kr.dataportal.application.persistence.entity
+package kr.dataportal.application.persistence.entity.account
 
 import kr.dataportal.application.persistence.config.jpa.BaseEntity
 import javax.persistence.Entity
